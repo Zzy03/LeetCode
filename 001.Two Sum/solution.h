3b847a49a040f284c1bae69985eb3f565be32d59
zzy <zzy351763136@gmail.com>
2017-08-03 15:43:26 +0800

    001

---
+++ 001.Two Sum/solution.h
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+    vector<int> twoSum(vector<int> &nums, int target) 
+    {
+        vector<int> result;
+        int N = nums.size();
+        for (int i = 0; i < N - 1; i++) 
+        {
+            for (int j = i+1; j < N; j++) 
+            {
+                if (nums[i] + nums[j] == target) 
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+    }
+};
